13d6689a17d172761de436b3a9b0a1e027a41b02
obinnaglory <yagazieobinna@gmail.com>
2025-09-09 08:23:17 +0100

    now

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically set current year
+document.getElementById("currentyear").textContent = new Date(2025, 9, 9).getFullYear();
+
+// Dynamically set last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
